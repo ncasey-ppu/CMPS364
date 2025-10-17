1386c820c6c1472597f7094225c3c3d8ab8a7f46
ncasey-ppu <ncasey@pointpark.edu>
2025-10-16 21:02:00 -0400

    Added db.js and updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log('app is listening to port 3000')
+let db
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => {
+        console.log('app is listening to port 3000')
+        })
+        db = getDb()
+    }
 })
 
 app.get('/Broadway', (req, res) => {
-    res.json({msg: "Welcome to Broadway"})
+    let musicals = []
+    db.collections('Musicals')
+    .find
+    .sort({ Title: 1 })
+    .forEach(musical => musicals.push(musical))
+    .then(() => {
+        res.status(200).json(musicals)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Could not fetch documents'})
+    })
+    //res.json({msg: "Welcome to Broadway"})
 })
